128e71af9683d2b268d0d5be87f7394b8fbbd551
ir.122052 <longhuiyantian@gmail.com>
2019-12-13 15:48:58 +0000

    for heroku

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,4 +1,6 @@
-
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
 
 if Rails.env.production?
   CarrierWave.configure do |config|
@@ -7,8 +9,11 @@ if Rails.env.production?
       provider               => 'AWS',
       aws_access_key_id      => ENV['S3_ACCESS_KEY'],
       aws_secret_access_key  => ENV['S3_SECRET_KEY'],
-      region                 => ENV['S3_REGION'],  # S3バケット作成時に指定したリージョン。バージニア北部
+      region                 => ENV['S3_REGION'] # S3バケット作成時に指定したリージョン。バージニア北部
     }
     config.fog_directory  = ENV['S3_BUCKET'] # 作成したS3バケット名
   end
+  
+  # 日本語ファイル名の設定
+  CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/ 
 end

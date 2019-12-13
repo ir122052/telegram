3e67dd69b45a64d6aa4b50f9e823982277e14652
ir.122052 <longhuiyantian@gmail.com>
2019-12-13 15:58:50 +0000

    for heroku

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,10 +6,10 @@ if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
-      provider               => 'AWS',
-      aws_access_key_id      => ENV['S3_ACCESS_KEY'],
-      aws_secret_access_key  => ENV['S3_SECRET_KEY'],
-      region                 => ENV['S3_REGION'] # S3バケット作成時に指定したリージョン。バージニア北部
+      :provider               => 'AWS',
+      :aws_access_key_id      => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key  => ENV['S3_SECRET_KEY'],
+      :region                 => ENV['S3_REGION'] # S3バケット作成時に指定したリージョン。バージニア北部
     }
     config.fog_directory  = ENV['S3_BUCKET'] # 作成したS3バケット名
   end

149b51e961071689f5b55876a3a4c464fa225d7a
ir.122052 <longhuiyantian@gmail.com>
2019-12-14 15:28:29 +0000

    for heroku

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,6 +5,7 @@ require 'carrierwave/storage/fog'
 if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_provider = 'fog/aws'
+    config.fog_public = false
     config.fog_credentials = {
       :provider               => 'AWS',
       :aws_access_key_id      => ENV['S3_ACCESS_KEY'],
